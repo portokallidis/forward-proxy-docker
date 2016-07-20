12abd140d1263bf407471ca88df0f23310bf781c
Nick Portokallidis <portokallidis@gmail.com>
2016-07-20 14:58:21 +0300

    fix conf

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ COPY . ./
 
 ENV PORT 80
 
+RUN rm /etc/nginx/conf.d/*.conf && mv /usr/share/nginx/html/nginx.conf.default /etc/nginx/conf.d/nginx.conf && nginx -t
+
 EXPOSE 80
 
 CMD ["bash","run.sh"]
--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # Preprend the upstream configuration
 
-rm /etc/nginx/conf.d/*.conf && mv /usr/share/nginx/html/nginx.conf.default /etc/nginx/conf.d/nginx.conf && nginx -t
-
 (echo "upstream theservice { server $SERVER_ADDR:$SERVER_PORT; }" && cat /etc/nginx/conf.d/nginx.conf) > proxy.conf.new
 mv proxy.conf.new /etc/nginx/conf.d/nginx.conf
 
@@ -10,4 +8,4 @@ mv proxy.conf.new /etc/nginx/conf.d/nginx.conf
 cat /etc/nginx/conf.d/nginx.conf
 
 # Start nginx
-nginx -g daemon off;
+nginx -g 'daemon off;'
